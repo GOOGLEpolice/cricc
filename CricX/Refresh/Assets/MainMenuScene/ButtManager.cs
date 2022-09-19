7fc34af64114cfaad0a9144248530f15f664f3cf
AbheekBit <81671530+AbheekBit@users.noreply.github.com>
2022-09-19 14:20:23 +0530

    Main Menu
    Added the main Menu scene and it's dependencies

---
+++ CricX/Refresh/Assets/MainMenuScene/ButtManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtManager : MonoBehaviour
+{
+    public GameObject setPan;
+    public GameObject buttPan;
+    public bool SFX;
+    public bool Music;
+     public void OnPlay()
+    {
+        SceneManager.LoadScene(sceneName: "GameScreen");
+    }
+
+    public void OnInven()
+    {
+        SceneManager.LoadScene(sceneName: "InventorySceen");
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit(0);
+    }
+
+    public void OnSetting()
+    {
+        setPan.SetActive(true);
+        buttPan.SetActive(false);
+    }
+
+    public void OnSetClose()
+    {
+        setPan.SetActive(false);
+        buttPan.SetActive(true);
+    }
+
+    public void OnSFX()
+    {
+        //SFX toggle code goes here
+        SFX = !SFX;
+        
+    }
+
+    public void OnMusic()
+    {
+        //Music toggle code goes here
+        Music = !Music;
+    }
+}
